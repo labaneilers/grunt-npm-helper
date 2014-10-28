4c6a32ef6c07a3dcf02423d6ddb837ddd34db548
Laban Eilers <laban@eilers.com>
2014-10-28 15:10:08 -0400

    Factoring out helper so it can be accessed without grunt

--- tasks/npm-helper.js
+++ tasks/npm-helper.js
@@ -1,46 +1,20 @@
 "use strict";
 
-var npm = require("npm");
-var npmconf = require("npm/lib/config/core.js");
-var configDefs = npmconf.defs;
-var shorthands = configDefs.shorthands;
-var types = configDefs.types;
-var nopt = require("npm/node_modules/nopt");
-var extend = require("util")._extend;
+var npmWrapper = require("../lib/npm-wrapper");
 
 module.exports = function (grunt) {
 
     grunt.registerMultiTask("npm", "Wrapper for npm", function () {
         var done = this.async();
 
-        // Use the same command line arg parsing as the actual npm cli
-        var conf = nopt(types, shorthands, [null, null].concat(this.data.args));
-        npm.argv = conf.argv.remain;
+        npmWrapper(this.data.args, this.options(), function (er) {
+            if (er) {
+                grunt.log.error(er);
+                done(false);
+                return;
+            }
 
-        // Grab the command
-        if (npm.deref(npm.argv[0])) {
-            npm.command = npm.argv.shift();
-        }
-
-        // Command line args should win over configuration
-        var coalescedConf = extend(this.options(), conf);
-
-        npm.load(
-            coalescedConf,
-            function (er) {
-                if (er) {
-                    grunt.log.error(er);
-                    done(false);
-                }
-
-                npm.commands[npm.command](npm.argv, function (er) {
-                    if (er) {
-                        grunt.log.error(er);
-                        done(false);
-                    }
-
-                    done();
-                });
-            });
+            done();
+        });
     });
 };
